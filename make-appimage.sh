1c8d566f3c7e15288f9b27925eca4905c0a3c7c9
Link4Electronics <link4electronics@gmail.com>
2026-04-11 21:07:52 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,8 +5,9 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/SpacingBat3/WebCord/refs/heads/master/sources/assets/icons/app.png
 export STARTUPWMCLASS=WebCord
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
@@ -15,7 +16,9 @@ export DEPLOY_VULKAN=1
 # Deploy dependencies
 quick-sharun ./AppDir/bin/*
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
